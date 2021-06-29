d269b93b1a88ace2390ece8dc1c82bd650b0b0f1
Michael Wright <wrimicha@sheridancollege.ca>
2021-06-29 16:19:14 -0400

    test

--- server.js
+++ server.js
@@ -4,49 +4,54 @@ const session = require('express-session');
 const cors = require('cors');
 const mysql = require('mysql2');
 const multer = require('multer');
+
 app.use(cors());
+
 const port = process.env.PORT || 3000;
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/uploads',express.static('uploads'));
+app.use('/uploads', express.static('uploads'));
+
 const storage = multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null,'uploads');
+    destination: function (req, file, cb) {
+        cb(null, 'uploads');
     },
-    filename: function(req,file,cb){
-        cb(null,Date.now() + file.originalname);
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname);
     }
 });
-const fileFilter = (req,file,cb) =>{
-    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
-        cb(null,true);
-    }else{
-        cb(null,false);
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
     }
 }
-const upload = multer({storage:storage, limits: {fileSize:1024*1024*5},fileFilter:fileFilter});
+
+const upload = multer({ storage: storage, limits: { fileSize: 1024 * 1024 * 5 }, fileFilter: fileFilter });
+
 const connection = mysql.createConnection({
-    host:'localhost',
-    user:'newuser',
-    password:'Secret123',
-    database:'recipes'
+    host: 'localhost',
+    user: 'newuser',
+    password: 'Secret123',
+    database: 'recipes'
 
 })
 
 
+app.get('/recipes', (req, res) => {
 
-app.get('/recipes',(req,res)=>{
- 
-    connection.query('SELECT * FROM recipe',(req,resp)=>{
+    connection.query('SELECT * FROM recipe', (req, resp) => {
         res.json(resp);
     });
 
 })
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("<h1>Hello Recipe API</h1>")
 })
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`the server is running on port ${port}`);
 });
