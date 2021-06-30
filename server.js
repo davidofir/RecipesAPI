b4e9db84c6bdcd32f5a6d0ea7b0f801dbcb47e97
ofir <davidof@sheridancollege.ca>
2021-06-30 16:07:11 +0300

    picture upload path

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const mysql = require('mysql2');
 app.use(cors());
 app.use(express.json());
+app.use('/uploads/',express.static('uploads'));
+const multer = require('multer');
+app.use(express.urlencoded({extended:true}));
 const port = process.env.PORT || 3000;
 const connection = mysql.createPool({
     host: 'us-cdbr-east-04.cleardb.com',
@@ -12,29 +15,27 @@ const connection = mysql.createPool({
     database: 'heroku_080948200542108',
     debug:'false'
 
-})
-// var con;
-// function handleDisconnect() {
-//     con = mysql.createConnection(connection); // Recreate the connection, since
-//                                                     // the old one cannot be reused.
-  
-//     con.connect(function(err) {              // The server is either down
-//       if(err) {                                     // or restarting (takes a while sometimes).
-//         console.log('error when connecting to db:', err);
-//         setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-//       }                                     // to avoid a hot loop, and to allow our node script to
-//     });                                     // process asynchronous requests in the meantime.
-//                                             // If you're also serving http, display a 503 error.
-//     con.on('error', function(err) {
-//       console.log('db error', err);
-//       if(err.code === 'PROTOCOL_CONNECTION_LOST') { // connection to the MySQL server is usually
-//         handleDisconnect();                         // lost due to either server restart, or a
-//       } else {                                      // connnection idle timeout (the wait_timeout
-//         throw err;                                 // server variable configures this)
-//       }
-//     });
-//   }
-// handleDisconnect();
+});
+
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,'uploads');
+    },
+    filename: function(req,file,cb){
+        cb(null,Date.now() + file.originalname);
+    }
+});
+
+const fileFilter = (req,file,cb) =>{
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+        cb(null,true);
+    }else{
+        cb(null,false);
+    }
+}
+
+const upload = multer({storage:storage, limits: {fileSize:1024*1024*5},fileFilter:fileFilter});
+
 app.get('/', (req, res) => {
     res.send("Hello Again!")
 });
@@ -48,6 +49,14 @@ app.get('/recipes', (req, res) => {
 
 })
 
+app.post("/upload",upload.fields([{name:"recipeImages",maxCount:5}]),(req,res,next)=>{
+    console.log(req.files);
+    req.files.forEach((file)=>{
+        connection.query('INSERT INTO recipeimg(recipeID,path) VALUES(?,?)',[5,file.path],(err,resp)=>{
+            if(err) throw err;
+        });
+    })
+})
 
 app.listen(port, () => {
     console.log(`the server is running on port ${port}`);
