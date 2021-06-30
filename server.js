aeed040147f93ae7208b0416563ae252a69d7d39
ofir <davidof@sheridancollege.ca>
2021-06-30 16:56:05 +0300

    fixed image uploading

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const mysql = require('mysql2');
-app.use(cors());
+const cors = require('cors');
+const multer = require('multer');
 app.use(express.json());
 app.use('/uploads/',express.static('uploads'));
-const multer = require('multer');
 app.use(express.urlencoded({extended:true}));
-const port = process.env.PORT || 3000;
-const connection = mysql.createPool({
-    host: 'us-cdbr-east-04.cleardb.com',
-    user: 'bad8b023ef60ea',
-    password: '2e405a5e',
-    database: 'heroku_080948200542108',
-    debug:'false'
+app.use(cors());
 
-});
+const port = process.env.PORT || 3000;
 
 const storage = multer.diskStorage({
     destination: function(req,file,cb){
         cb(null,'uploads');
     },
     filename: function(req,file,cb){
-        cb(null,Date.now() + file.originalname);
+        cb(null, file.originalname);
     }
 });
 
@@ -35,6 +28,19 @@ const fileFilter = (req,file,cb) =>{
 }
 
 const upload = multer({storage:storage, limits: {fileSize:1024*1024*5},fileFilter:fileFilter});
+
+const connection = mysql.createPool({
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: 'bad8b023ef60ea',
+    password: '2e405a5e',
+    database: 'heroku_080948200542108',
+    debug:'false'
+
+});
+
+app.listen(port, () => {
+    console.log(`the server is running on port ${port}`);
+});
 
 app.get('/', (req, res) => {
     res.send("Hello Again!")
@@ -49,15 +55,8 @@ app.get('/recipes', (req, res) => {
 
 })
 
-app.post("/upload",upload.fields([{name:"recipeImages",maxCount:5}]),(req,res,next)=>{
-    console.log(req.files);
-    req.files.forEach((file)=>{
-        connection.query('INSERT INTO recipeimg(recipeID,path) VALUES(?,?)',[5,file.path],(err,resp)=>{
-            if(err) throw err;
-        });
-    })
+app.post("/upload",upload.fields([{name:"imgs",maxCount:5}]),(req,res,next)=>{
+    console.log(req.files.imgs);
+    res.sendStatus(200);
 })
 
-app.listen(port, () => {
-    console.log(`the server is running on port ${port}`);
-});
