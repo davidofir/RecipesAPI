4ada88827a928955690ab18f2c9fe1a00a34d58f
ofir <davidof@sheridancollege.ca>
2021-06-30 12:45:42 +0300

    fixed the db

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const mysql = require('mysql2');
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 3000;
+//mysql://bad8b023ef60ea:2e405a5e@us-cdbr-east-04.cleardb.com/heroku_080948200542108?reconnect=true
+const connection = mysql.createConnection({
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: 'bad8b023ef60ea',
+    password: '2e405a5e',
+    database: 'heroku_080948200542108'
 
-
+})
 
 app.get('/', (req, res) => {
     res.send("Hello Again!")
@@ -15,7 +21,9 @@ app.get('/', (req, res) => {
 
 app.get('/recipes', (req, res) => {
 
-    res.json({recipe:"pasta"})
+    connection.query('SELECT * FROM recipe', (req, resp) => {
+        res.json(resp);
+    });
 
 })
 
