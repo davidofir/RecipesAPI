89982a34aca79b9bac7312e9dd12bf88377e854b
ofir <davidof@sheridancollege.ca>
2021-06-30 13:11:04 +0300

    error handling connection

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const mysql = require('mysql2');
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 3000;
-//mysql://bad8b023ef60ea:2e405a5e@us-cdbr-east-04.cleardb.com/heroku_080948200542108?reconnect=true
 const connection = mysql.createConnection({
     host: 'us-cdbr-east-04.cleardb.com',
     user: 'bad8b023ef60ea',
@@ -13,6 +12,27 @@ const connection = mysql.createConnection({
     database: 'heroku_080948200542108'
 
 })
+var con;
+function handleDisconnect() {
+    con = mysql.createcon(connection); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+  
+    con.connect(function(err) {              // The server is either down
+      if(err) {                                     // or restarting (takes a while sometimes).
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    con.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // connection to the MySQL server is usually
+        handleDisconnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+    });
+  }
 
 app.get('/', (req, res) => {
     res.send("Hello Again!")
