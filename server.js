ee0ceab7ab2ea50a479ae2d11d2d21ca0e417ae6
ofir <davidof@sheridancollege.ca>
2021-06-30 10:32:19 +0300

    testing

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
-//const importData = require("./data.json");
+const cors = require('cors');
+const mysql = require('mysql2');
+app.use(cors());
 const port = process.env.PORT || 3000;
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'newuser',
+    password: 'Secret123',
+    database: 'recipes'
+
+})
+
 
 app.get('/', (req, res) => {
     res.send("Hello Again!")
@@ -9,8 +20,12 @@ app.get('/', (req, res) => {
 
 
 app.get('/recipes', (req, res) => {
-    res.send('recipes');
-});
+
+    connection.query('SELECT * FROM recipe', (req, resp) => {
+        res.json(resp);
+    });
+
+})
 
 
 app.listen(port, () => {
