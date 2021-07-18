262b52eaca1b6ffa66d90c1cb85d3132fd021063
Michael Wright <wrimicha@sheridancollege.ca>
2021-07-18 14:47:44 -0400

    testing old code

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,6 +1,121 @@
+// const express = require('express');
+// const router = express.Router();
+// const mysql = require('mysql2');
+
+// const connection = mysql.createPool({
+//     host: 'us-cdbr-east-04.cleardb.com',
+//     user: 'bad8b023ef60ea',
+//     password: '2e405a5e',
+//     database: 'heroku_080948200542108',
+//     debug:'false'
+// });
+
+// // var connection = mysql.createConnection({
+// //     host: 'localhost',
+// //     user: 'root',
+// //     password: 'mike_J97w',
+// //     database: 'recipeapp'
+// // });
+
+// router.get('/', (req, res) => {
+//     connection.query('SELECT * FROM recipe', (req, resp) => {
+//         res.json(resp);
+//     })
+//     //console.log("got recipes")
+// })
+
+// router.get('/sorted', (req, res) => {
+//     connection.query('SELECT * FROM recipe ORDER BY creation_date DESC', (req, resp) => {
+//         res.json(resp);
+//     });
+// })
+
+// router.get('/sortedRating', (req, res) => {
+//     connection.query('SELECT * FROM recipe ORDER BY rating DESC', (req, resp) => {
+//         res.json(resp);
+//     });
+// })
+
+// router.get('/imgs',(req,res)=>{
+//     //res.send("IMGS");
+//     connection.query('SELECT * FROM recipeimg',(req,resp)=>{
+//         res.json(resp)
+//     })
+// })
+
+// router.get('/:id',(req,res)=>{
+//     var recipe;
+//     connection.query('SELECT * FROM recipe WHERE (recipe.recipeID=?)',[req.params.id],(err,response)=>{
+//         if(err) throw err;
+//         recipe = response;
+//         res.json(recipe);
+//     })
+// })
+
+// router.post('/recipes',(req,res,next)=>{
+//     connection.query('INSERT INTO recipe(title,rating,cooktime,instructions) VALUES("FOOD 2","5","30","COOK THEM!");')
+//     // connection.query('INSERT INTO recipe(title,rating,cooktime,instructions) VALUES(?,?,?,?)',[req.body.title,req.body.rating,req.body.cooktime,req.body.instructions],
+//     // (err,resp)=>{
+//     //     if(err) throw err;
+//     //     next();
+//     // })
+// })
+
+// // ,
+// // (req,res,next)=>{
+// //     connection.query('SELECT LAST_INSERT_ID()',(err,result)=>{
+// //         console.log(result[0]['LAST_INSERT_ID()']);
+// //         req.lastID = result[0]['LAST_INSERT_ID()'];
+// //         next();
+// //     })
+// // },
+// // (req,res)=>{
+// //     for(let i = 0; i < req.body.ingredients.length; i++){
+// //         console.log(req.lastID);
+// //         connection.query('INSERT INTO ingredient(recipeID,name,qty,unit) VALUES (?,?,?,?)',[req.lastID,req.body.ingredients[i]["name"],req.body.ingredients[i]["qty"],req.body.ingredients[i]["unit"]],(err,result)=>{
+// //         res.end();
+// //         })
+// //         }
+// // }
+// // )
+
+// module.exports = router;
+
+
+
+
+//old
+
 const express = require('express');
-const router = express.Router();
+const app = express();
 const mysql = require('mysql2');
+const cors = require('cors');
+const multer = require('multer');
+app.use(express.json());
+app.use('/uploads/',express.static('uploads'));
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,'uploads');
+    },
+    filename: function(req,file,cb){
+        cb(null, file.originalname);
+    }
+});
+
+const fileFilter = (req,file,cb) =>{
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+        cb(null,true);
+    }else{
+        cb(null,false);
+    }
+}
+
+const upload = multer({storage:storage, limits: {fileSize:1024*1024*5},fileFilter:fileFilter});
 
 const connection = mysql.createPool({
     host: 'us-cdbr-east-04.cleardb.com',
@@ -8,42 +123,43 @@ const connection = mysql.createPool({
     password: '2e405a5e',
     database: 'heroku_080948200542108',
     debug:'false'
+
 });
 
-// var connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'mike_J97w',
-//     database: 'recipeapp'
-// });
+app.listen(port, () => {
+    console.log(`the server is running on port ${port}`);
+});
 
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
+    res.send("Hello Again!")
+});
+
+
+app.get('/recipes', (req, res) => {
+
     connection.query('SELECT * FROM recipe', (req, resp) => {
         res.json(resp);
-    })
-    //console.log("got recipes")
-})
-
-router.get('/sorted', (req, res) => {
-    connection.query('SELECT * FROM recipe ORDER BY creation_date DESC', (req, resp) => {
-        res.json(resp);
     });
-})
 
-router.get('/sortedRating', (req, res) => {
-    connection.query('SELECT * FROM recipe ORDER BY rating DESC', (req, resp) => {
-        res.json(resp);
-    });
 })
-
-router.get('/imgs',(req,res)=>{
-    //res.send("IMGS");
+app.get('/recipes/imgs',(req,res)=>{
     connection.query('SELECT * FROM recipeimg',(req,resp)=>{
         res.json(resp)
     })
 })
+// app.post("/upload",upload.fields([{name:"imgs",maxCount:5}]),(req,res,next)=>{
+//     console.log(req.files.imgs);
 
-router.get('/:id',(req,res)=>{
+//     req.files.imgs.foreach((file)=>{
+//         connection.query('INSERT INTO recipeimg(recipeID,path) VALUES(?,?)',[5,file.path],(err,response)=>{
+//             if(err) throw err;
+//         })
+//     })
+
+//     res.sendStatus(200);
+// })
+
+app.get('/recipes/:id',(req,res)=>{
     var recipe;
     connection.query('SELECT * FROM recipe WHERE (recipe.recipeID=?)',[req.params.id],(err,response)=>{
         if(err) throw err;
@@ -51,32 +167,37 @@ router.get('/:id',(req,res)=>{
         res.json(recipe);
     })
 })
-
-router.post('/recipes/add',(req,res,next)=>{
-    connection.query('INSERT INTO recipe(title,rating,cooktime,instructions) VALUES("FOOD 2","5","30","COOK THEM!");')
-    // connection.query('INSERT INTO recipe(title,rating,cooktime,instructions) VALUES(?,?,?,?)',[req.body.title,req.body.rating,req.body.cooktime,req.body.instructions],
-    // (err,resp)=>{
-    //     if(err) throw err;
-    //     next();
-    // })
+app.get('/ingredients/:id',(req,res)=>{
+    connection.query('SELECT * FROM ingredient WHERE (recipeID=?)',[req.params.id],(err,response)=>{
+        if(err) throw err;
+        res.json(response);
+    })
 })
-
-// ,
-// (req,res,next)=>{
-//     connection.query('SELECT LAST_INSERT_ID()',(err,result)=>{
-//         console.log(result[0]['LAST_INSERT_ID()']);
-//         req.lastID = result[0]['LAST_INSERT_ID()'];
-//         next();
-//     })
-// },
-// (req,res)=>{
-//     for(let i = 0; i < req.body.ingredients.length; i++){
-//         console.log(req.lastID);
-//         connection.query('INSERT INTO ingredient(recipeID,name,qty,unit) VALUES (?,?,?,?)',[req.lastID,req.body.ingredients[i]["name"],req.body.ingredients[i]["qty"],req.body.ingredients[i]["unit"]],(err,result)=>{
-//         res.end();
-//         })
-//         }
-// }
-// )
-
-module.exports = router;
+app.get('/ingredients',(req,res)=>{
+    connection.query('SELECT * FROM ingredient',(err,response)=>{
+        if(err) throw err;
+        res.json(response);
+    })
+})
+app.post('/recipes',(req,res,next)=>{
+    connection.query('INSERT INTO recipe(title,rating,cooktime,instructions) VALUES(?,?,?,?)',[req.body.title,req.body.rating,req.body.cooktime,req.body.instructions],
+    (err,resp)=>{
+        if(err) throw err;
+        next();
+    })
+},(req,res,next)=>{
+    connection.query('SELECT LAST_INSERT_ID()',(err,result)=>{
+        console.log(result[0]['LAST_INSERT_ID()']);
+        req.lastID = result[0]['LAST_INSERT_ID()'];
+        next();
+    })
+}
+,(req,res)=>{
+    for(let i = 0; i < req.body.ingredients.length; i++){
+        console.log(req.lastID);
+        connection.query('INSERT INTO ingredient(recipeID,name,qty,unit) VALUES (?,?,?,?)',[req.lastID,req.body.ingredients[i]["name"],req.body.ingredients[i]["qty"],req.body.ingredients[i]["unit"]],(err,result)=>{
+        res.end();
+        })
+        }
+}
+)
