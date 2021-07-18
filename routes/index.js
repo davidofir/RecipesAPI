49836a0d784bfa86a2836449d4d1c3c6287950ac
Michael Wright <wrimicha@sheridancollege.ca>
2021-07-17 20:40:18 -0400

    test

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+const mysql = require('mysql2');
+
+const connection = mysql.createPool({
+  host: 'us-cdbr-east-04.cleardb.com',
+  user: 'bad8b023ef60ea',
+  password: '2e405a5e',
+  database: 'heroku_080948200542108',
+  debug:'false'
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  connection.query('SELECT * FROM recipe', (req, resp) => {
+    res.json(resp);
+  });
+});
+
+module.exports = router;
