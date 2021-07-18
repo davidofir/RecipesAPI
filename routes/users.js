57d3a56eda9ca83dce357b056553adce2cfb3663
Michael Wright <wrimicha@sheridancollege.ca>
2021-07-17 21:28:21 -0400

    test

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with resource');
   alert('respond with a resource');
   console.log('respond with a resource');
 });
